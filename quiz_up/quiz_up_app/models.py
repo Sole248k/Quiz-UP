f28c7465fd914bb4dab27d40a5ce7aac377fedef
Sole248k <248klontac@gmail.com>
2024-06-26 14:04:28 +0800

    initial model and form

--- quiz_up/quiz_up_app/models.py
+++ quiz_up/quiz_up_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=16)
+    about_user = models.TextField(default='')
--- quiz_up/quiz_up_app/views.py
+++ quiz_up/quiz_up_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def landing(request):
